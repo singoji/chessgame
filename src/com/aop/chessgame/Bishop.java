8d9b68059064657f049781e98115228263fa80a3
hari <hari.damerla@gmail.com>
2016-04-05 09:50:21 +0530

    Rook.java

--- src/com/aop/chessgame/Bishop.java
+++ src/com/aop/chessgame/Bishop.java
@@ -8,6 +8,34 @@ public class Bishop extends Coin {
 	@Override
 	public boolean canMove(int dest, ChessGame game) {
 		// TODO Auto-generated method stub
+		if (isDiagnalAndFree(getPosition(), dest, game)) {
+			return true;
+		}
+
 		return false;
+	}
+
+	private boolean isDiagnalAndFree(int position, int dest, ChessGame game) {
+		int currentRow = getRow(position);
+		int currentColumn = getColumn(position);
+		int destRow = getRow(dest);
+		int destColumn = getColumn(dest);
+		if (Math.abs(currentRow - destRow) == Math.abs(currentColumn - destColumn)) {
+			for (int x = Math.min(currentColumn, destColumn) + 1; x < Math.max(currentColumn, destColumn) - 1; x++) {
+				for (int y = Math.min(currentRow, destRow) + 1; y < Math.max(currentRow, destRow) - 1; y++) {
+					// make sure all are free
+					if (game.getCoin(getIndex(x, y)) != null) {
+						return false;
+					}
+				}
+			}
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private int getIndex(int row, int column) {
+		return row * 8 + column;
 	}
 }
--- src/com/aop/chessgame/Knight.java
+++ src/com/aop/chessgame/Knight.java
@@ -8,6 +8,42 @@ public class Knight extends Coin {
 	@Override
 	public boolean canMove(int dest, ChessGame game) {
 		// TODO Auto-generated method stub
+		if (isStrightAndFree(getPosition(), dest, game)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private boolean isStrightAndFree(int position, int dest, ChessGame game) {
+		// TODO Auto-generated method stub
+		int currentRow = getRow(position);
+		int currentColumn = getColumn(position);
+		int destRow = getRow(dest);
+		int destColumn = getColumn(dest);
+		if (currentRow + 1 == destRow || currentRow - 1 == destRow) {
+			if (game.getCoin(getIndex(destRow, destColumn)) != null) {
+				return false;
+
+			} else {
+				// nothing in between
+				return true;
+			}
+		}
+		if (currentColumn + 1 == destColumn || currentColumn - 1 == destColumn) {
+			if (game.getCoin(getIndex(destRow, destColumn)) != null) {
+
+				return true;
+			} else {
+				return false;
+
+			}
+		}
 		return false;
+
+	}
+
+	private int getIndex(int row, int column) {
+		return row * 8 + column;
 	}
 }
